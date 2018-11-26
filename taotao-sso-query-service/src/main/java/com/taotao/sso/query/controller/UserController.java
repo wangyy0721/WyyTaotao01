9359c61b1d302a839c07034f555c2b505861887c
1606137163 <1606137163@qq.com>
2018-11-26 23:07:25 +0800

    1

---
+++ taotao-sso-query-service/src/main/java/com/taotao/sso/query/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.taotao.sso.query.controller;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.com.pojo.TaotaoResult;
+import com.taotao.com.utils.ExceptionUtil;
+import com.taotao.sso.query.api.UserQueryService;
+
+@RequestMapping("user")
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserQueryService userQueryService;
+
+    /**
+     * 根据token查询用户信息
+     * 
+     * @param token
+     * @return
+     */
+    @RequestMapping("/token/{token}")
+	@ResponseBody
+	public Object getUserByToken(@PathVariable String token, String callback) {
+		TaotaoResult result = null;
+		try {
+			result = userQueryService.getUserByToken(token);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
+		}
+
+		// 判断是否为jsonp调用
+		if (StringUtils.isBlank(callback)) {
+			return result;
+		} else {
+			MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(result);
+			mappingJacksonValue.setJsonpFunction(callback);
+			return mappingJacksonValue;
+		}
+	}
+
+}
---
+++ taotao-sso-query-service/src/main/java/com/taotao/sso/query/dao/JedisClient.java
@@ -0,0 +1,14 @@
+package com.taotao.sso.query.dao;
+
+public interface JedisClient {
+        String get(String key);
+        String set(String key,String value);
+        String hget(String hkey,String key);
+        long hset(String hkey,String key,String value);
+        long incr(String key);//自增长
+        long expire(String key,int second);//设置过期时间
+        long ttl(String key);//查看是否过期 -1 正常，-2过期
+        long del(String key);//清空缓存
+		long hdel(String hkey, String key);	
+		
+}
